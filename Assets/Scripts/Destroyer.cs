3fcbea889a9a46bc84af161e753de617e2e0ea67
Daniel Albarral <albarralnunez AT gmail DOT com>
2014-04-27 19:01:19 +0200

    Pathfinding

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (GameObject.Find ("Target").transform.position == GameObject.transform.position)
+//			Destroy(gameObject,0.0F);
+	}
+}
---
+++ Assets/Scripts/SpawnPointBeheibur.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPointBeheibur : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", 5,5);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Spawn() {
+		GameObject go = (GameObject)Instantiate(Resources.Load<GameObject> ("Bot"));
+		Pathfinding.MineBotAI comp = go.GetComponent<Pathfinding.MineBotAI>();
+		comp.target = GameObject.Find("Target").transform;		
+	}
+}
